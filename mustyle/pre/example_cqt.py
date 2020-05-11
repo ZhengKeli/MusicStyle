77ac83c64f116f23b9fefcf3fcd7d8f2b4a06bab
ZhengKeli <zhengkeli2009@126.com>
2020-05-11 16:14:09 +0800

    added example of cqt

---
+++ mustyle/pre/example_cqt.py
@@ -0,0 +1,21 @@
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+
+from mustyle.io.io import load_audio
+
+sample_rate = 22050
+mp3_filename = r"C:\Users\keli\OneDrive\学习\CIS Machine Learning\MusicStyle\data\genres\blues\blues.00000.wav"
+
+wave = load_audio(mp3_filename, sample_rate)
+plt.plot(wave)
+plt.show()
+
+cqt = librosa.cqt(wave, sample_rate)
+cqt = librosa.amplitude_to_db(cqt, ref=np.max)
+
+plt.figure(figsize=(10, 3))
+librosa.display.specshow(cqt, y_axis='cqt_note')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Constant-Q power spectrogram (note)')
+plt.show()
