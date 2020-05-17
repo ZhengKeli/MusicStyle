eb34a5ec60b914717e484cbf78c13ed93c55b885
ZhengKeli <zhengkeli2009@126.com>
2020-05-18 00:16:03 +0800

    deleted useless files

--- unittests/test_model.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from model.resnet import Resnet34_modified
-
-
-class TestDatasetAudio(unittest.TestCase):
-    def test_resnet34_modified(self):
-        model = Resnet34_modified((84, 1290, 1), 10)
-        print(model)
