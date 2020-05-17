3ce3e4bd5e0bb9225f6c81baf2c9d798741b2782
ZhengKeli <zhengkeli2009@126.com>
2020-05-17 14:23:53 +0800

    fixed a bug in scanning

--- split.py
+++ split.py
@@ -1,7 +1,6 @@
 import numpy as np
 import os
-from dataset.dataset import flatten_dataset, scan_dataset, split_dataset
-from dataset.splitting import save_cls_file, save_ref_file
+from dataset.splitting import flatten_dataset, save_cls_file, save_ref_file, scan_dataset, split_dataset
 
 # configurations
 seed = np.random.randint(100000)
@@ -10,21 +9,27 @@ np.random.seed(seed)
 
 # scan
 dataset = scan_dataset(dataset_dir)
-train_set, test_set, valid_set = split_dataset(dataset, (3, 1, 1))
+print('scanned dataset')
 
+train_set, test_set, valid_set = split_dataset(dataset, (3, 1, 1))
 classes, train_set = flatten_dataset(train_set)
 _, test_set = flatten_dataset(test_set)
 _, valid_set = flatten_dataset(valid_set)
+print('split dataset')
 
 # save
 classes_fn = os.path.join(dataset_dir, 'classes.txt')
 save_cls_file(classes, classes_fn)
+print('saved class file to', classes_fn)
 
 train_set_fn = os.path.join(dataset_dir, "train_set.txt")
 save_ref_file(train_set, train_set_fn)
+print('saved train set ref file to', train_set_fn)
 
 test_set_fn = os.path.join(dataset_dir, "test_set.txt")
 save_ref_file(test_set, test_set_fn)
+print('saved test set ref file to', test_set_fn)
 
 valid_set_fn = os.path.join(dataset_dir, "valid_set.txt")
 save_ref_file(valid_set, valid_set_fn)
+print('saved valid set ref file to', valid_set_fn)
